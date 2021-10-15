adc8062d89b997416a8d486b399fdfeb34f5c21a
EdersonRodrigues7 <ederson@trilhante.com.br>
2021-10-15 11:23:31 -0300

    correcting styles

--- src/ui/components/data-display/PageTitle/PageTitle.style.tsx
+++ src/ui/components/data-display/PageTitle/PageTitle.style.tsx
@@ -8,7 +8,7 @@ export const PageTitleContainer = styled.View`
 export const PageTitleStyled = styled.Text`
   margin: 0;
   color: ${({ theme }) => theme.colors.primary};
-  font-size: 1rem;
+  font-size: 16px;
   text-align: center;
   font-weight: bold;
 `;
@@ -16,7 +16,7 @@ export const PageTitleStyled = styled.Text`
 export const PageSubtitleStyled = styled.Text`
   margin: ${({ theme }) => theme.spacing(1.5) + ' '};
   color: ${({ theme }) => theme.colors.text};
-  font-size: 0.8rem;
+  font-size: 12px;
   text-align: center;
   font-weight: normal;
   max-width: 275px;
